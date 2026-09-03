97d5c539581b1b8c62c09591863a676fbe075079
Toluwani Somade <tsomade.2200314@stu.cu.edu.ng>
2026-09-03 08:19:04 +0100

    feat: add new stimulus components for analytics, checkout, flight, music, and weather; enhance utility functions

--- day-3-blink/src/lib/utils.ts
+++ day-3-blink/src/lib/utils.ts
@@ -1,3 +1,6 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export type TemplateType =
   | "analytics"
   | "checkout"
@@ -68,6 +71,10 @@ export type StoredStats = {
   shortestThreshold: number;
   sessionsPlayed: number;
 };
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 export function randomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
