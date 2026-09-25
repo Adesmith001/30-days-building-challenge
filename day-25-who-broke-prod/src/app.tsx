879b42fabda610a51ea5d0cb205fb8841bd1507c
Toluwani Somade <tsomade.2200314@stu.cu.edu.ng>
2026-09-25 14:48:39 +0100

    chore: scaffold day 25 who broke prod

---
+++ day-25-who-broke-prod/src/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="grid min-h-screen place-items-center bg-[#090b0d] text-zinc-100">
+      <p className="font-mono text-xs tracking-[0.22em]">WHO BROKE PROD?</p>
+    </main>
+  );
+}
